7a2c5ce5e5cef10984bf3ae993a011177b1109e5
cdj5492 <cdj5492@rit.edu>
2022-04-09 18:06:03 -0400

    Update displayInterface.py

--- Display-Main/displayInterface.py
+++ Display-Main/displayInterface.py
@@ -17,6 +17,7 @@ def main():
         for y in range(8):
             print(A.getPixel(x, y))
             pixels[x+y*displayWidth] = A.getPixel(x, y)
+    neopixel.fill((0,0,255))
 
 if __name__ == '__main__':
     main()
