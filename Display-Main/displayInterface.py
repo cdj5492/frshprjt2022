3c21c52fe60238573500e5b03b4be09e3c763940
cdj5492 <cdj5492@rit.edu>
2022-04-09 17:53:55 -0400

    Update displayInterface.py

--- Display-Main/displayInterface.py
+++ Display-Main/displayInterface.py
@@ -13,8 +13,8 @@ pixels = neopixel.NeoPixel(board.D18, displayWidth * displayHeight)
 def main():
     dictionaryBuild.fullDictBuild()
     A = dictionaryBuild.cDict['A']
-    for x in range(8):
-        for y in range(8):
+    for x in range(7):
+        for y in range(7):
             pixels[x+y*displayWidth] = A.getPixel(x, y)
 
 if __name__ == '__main__':
