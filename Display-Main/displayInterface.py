03d2043cb791127e3bb32f7d296b9b465e78ce66
cdj5492 <cdj5492@rit.edu>
2022-04-09 18:22:27 -0400

    Update displayInterface.py

--- Display-Main/displayInterface.py
+++ Display-Main/displayInterface.py
@@ -15,8 +15,8 @@ def main():
     A = dictionaryBuild.cDict['A']
     for x in range(A.getWidth()):
         for y in range(8):
-            print(A.getPixel(x, y))
-            pixels[x+y*displayWidth] = A.getPixel(x, y)
+            print(A.getPixel(y, x))
+            pixels[x+y*displayWidth] = A.getPixel(y, x)
     pixels.fill((255,255,255))
 
 if __name__ == '__main__':
